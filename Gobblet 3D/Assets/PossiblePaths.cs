794f45ee96063c3da0c21a0fde0a4cfaa7f87edd
Charlie Wensink <cpwensink@gmail.com>
2022-10-03 17:30:40 -0400

    Make a piece gobble a smaller piece

--- Gobblet 3D/Assets/PossiblePaths.cs
+++ Gobblet 3D/Assets/PossiblePaths.cs
@@ -7,9 +7,11 @@ public class PossiblePaths : MonoBehaviour
     Ray ray;
     RaycastHit hit;
     public Camera mainCamera;
+    public Positions positionsScript;
+
     public static GameObject movablePiece;
     public string name;
-
+    public bool isPiece;
     public void Start()
     {
         name = this.gameObject.name;
@@ -35,13 +37,17 @@ public class PossiblePaths : MonoBehaviour
         {
             if (hit.transform.gameObject == this.gameObject)
             {
-                GetComponent<MeshRenderer>().enabled = true;
-                if(Input.GetMouseButtonDown(0))
+                CheckForPiece(int.Parse(name.Substring(0, 1)), int.Parse(name.Substring(2, 1)));
+                if (isPiece == false)
                 {
-                    movablePiece.GetComponent<Piece>().xPos = int.Parse(name.Substring(0, 1));
-                    movablePiece.GetComponent<Piece>().yPos = int.Parse(name.Substring(2, 1));
-                    movablePiece.GetComponent<PieceMovement>().isSelected = false;
-                    GetComponent<MeshRenderer>().enabled = false;
+                    GetComponent<MeshRenderer>().enabled = true;
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        movablePiece.GetComponent<Piece>().xPos = int.Parse(name.Substring(0, 1));
+                        movablePiece.GetComponent<Piece>().yPos = int.Parse(name.Substring(2, 1));
+                        movablePiece.GetComponent<PieceMovement>().isSelected = false;
+                        GetComponent<MeshRenderer>().enabled = false;
+                    }
                 }
             }
             if (hit.transform.gameObject != this.gameObject)
@@ -54,6 +60,27 @@ public class PossiblePaths : MonoBehaviour
             GetComponent<MeshRenderer>().enabled = false;
         }
     }
-
-
+    public void CheckForPiece(int x, int y)
+    {
+        int piece = 0;
+        foreach (Vector2 i in positionsScript.piecePositions)
+        {
+            if (x == i.x && y == i.y)
+            {
+                if (movablePiece.GetComponent<Piece>().size > positionsScript.pieces[piece].GetComponent<Piece>().size)
+                {
+                    isPiece = false;
+                    return;
+                }
+                else
+                {
+                    isPiece = true;
+                    return;
+                }
+            }
+            piece++;
+        }
+        isPiece = false;
+        return;
+    }
 }
